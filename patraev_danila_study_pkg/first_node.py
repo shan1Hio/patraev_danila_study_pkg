b6e4534a1033ada084876db0c872c1efcc73a00d
Danila <danila.email@example.com>
2026-04-14 11:08:58 +0300

    First task added

--- patraev_danila_study_pkg/first_node.py
+++ patraev_danila_study_pkg/first_node.py
@@ -1,13 +1,18 @@
-#!/usr/bin/env python3
-"""Первый узел ROS 2 — Hello World"""
+"""Timer"""
 
 import rclpy
+import time
 from rclpy.node import Node
 
 def main(args=None):
     rclpy.init(args=args)                   # инициализация ROS 2
-    node = Node('hello_node')               # создаём узел с именем hello_node
-    node.get_logger().info("Hello ROS 2 World! 🚀")
+    node = Node('Time_printer')               # создаём узел с именем hello_node
+    node.get_logger().info("Timer has started")
+    def timer_callback():
+        current_time = time.localtime()
+        time_str = time.strftime("%Y-%m-%d %H:%M:%S", current_time)
+        node.get_logger().info(f"Current time: {time_str}")
+    timer = node.create_timer(5.0, timer_callback)
     rclpy.spin(node)                        # запускаем цикл обработки
     node.destroy_node()
     rclpy.shutdown()
