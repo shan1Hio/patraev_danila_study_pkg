dcc26846750991955abac71ee8d604f785813934
Danila <danila.email@example.com>
2026-05-12 12:28:23 +0300

    Added 2nd task of lab

--- patraev_danila_study_pkg/listener.py
+++ patraev_danila_study_pkg/listener.py
@@ -1,78 +1,34 @@
-# #!/usr/bin/env python3
-# import rclpy                        
-# from rclpy.node import Node         
-# from std_msgs.msg import String     
-
-# class Listener(Node):
-
-#     def __init__(self):
-#         super().__init__('listener')
-
-#         self.subscription = self.create_subscription(
-#             String,
-#             'my_chat_topic',
-#             self.callback,
-#             10)
-
-#         self.get_logger().info("Узел listener запущен и слушает топик!")
-
-#     def callback(self, msg):
-#         self.get_logger().info(f"Я услышал: {msg.data}")
-
-# def main():
-#     rclpy.init()                    
-#     node = Listener()              
-#     try:
-#         rclpy.spin(node)            
-#     except KeyboardInterrupt:
-#         pass                        
-#     finally:
-#         node.destroy_node()        
-#         rclpy.shutdown()          
-
-# if __name__ == '__main__':
-#     main()
-
 #!/usr/bin/env python3
 import rclpy                        
-from rclpy.node import Node        
-from std_msgs.msg import Int32    
+from rclpy.node import Node         
+from std_msgs.msg import String     
 
-class overflow_l(Node):
+class Listener(Node):
 
     def __init__(self):
-        super().__init__('overflow_l')
-
-        self.declare_parameter('overflow_threshold', 80) 
-        self.declare_parameter('topic_name_2', 'overflow')
-        self.declare_parameter('node_name_2', 'overflow_l')
-
-        self.threshold = self.get_parameter('overflow_threshold').value
-        self.topic_2 = self.get_parameter('topic_name_2').value
-        self.node_2 = self.get_parameter('node_name_2').value
+        super().__init__('listener')
 
         self.subscription = self.create_subscription(
-            Int32,
-            self.topic_2,
+            String,
+            'my_chat_topic',
             self.callback,
             10)
 
-        self.get_logger().info(f'Узел {self.node_2} запущен и слушает топик!')
+        self.get_logger().info("Узел listener запущен и слушает топик!")
 
-   
     def callback(self, msg):
-        self.get_logger().info(f"[WARN] [overflow_l]:!!! OVERFLOW !!! Resulting number is {msg.data}")
+        self.get_logger().info(f"Я услышал: {msg.data}")
 
 def main():
-    rclpy.init()                   
-    node = overflow_l()              
+    rclpy.init()                    
+    node = Listener()              
     try:
         rclpy.spin(node)            
     except KeyboardInterrupt:
         pass                        
     finally:
         node.destroy_node()        
-        rclpy.shutdown()            
+        rclpy.shutdown()          
 
 if __name__ == '__main__':
     main()
--- patraev_danila_study_pkg/overflow_listener.py
+++ patraev_danila_study_pkg/overflow_listener.py
@@ -1,33 +1,77 @@
+# #!/usr/bin/env python3
+# import rclpy                       
+# from rclpy.node import Node         
+# from std_msgs.msg import Int32    
+
+# class overflow_l(Node):
+
+#     def __init__(self):
+#         super().__init__('overflow_l')
+#         self.subscription = self.create_subscription(
+#             Int32,
+#             'overflow',
+#             self.callback,
+#             10)
+
+#         self.get_logger().info("Узел overflow_l запущен и слушает топик!")
+
+   
+#     def callback(self, msg):
+#         self.get_logger().info(f"[WARN] [overflow_l]:!!! OVERFLOW !!! Resulting number is {msg.data}")
+
+# def main():
+#     rclpy.init()                    
+#     node = overflow_l()            
+#     try:
+#         rclpy.spin(node)           
+#     except KeyboardInterrupt:
+#         pass                        
+#     finally:
+#         node.destroy_node()         
+#         rclpy.shutdown()            
+
+# if __name__ == '__main__':
+#     main()
+
 #!/usr/bin/env python3
-import rclpy                       
-from rclpy.node import Node         
+import rclpy                        
+from rclpy.node import Node        
 from std_msgs.msg import Int32    
 
 class overflow_l(Node):
 
     def __init__(self):
         super().__init__('overflow_l')
+
+        self.declare_parameter('overflow_threshold', 80) 
+        self.declare_parameter('topic_name_2', 'overflow')
+        self.declare_parameter('node_name_2', 'overflow_l')
+
+        self.threshold = self.get_parameter('overflow_threshold').value
+        self.topic_2 = self.get_parameter('topic_name_2').value
+        self.node_2 = self.get_parameter('node_name_2').value
+
         self.subscription = self.create_subscription(
             Int32,
-            'overflow',
+            self.topic_2,
             self.callback,
             10)
 
-        self.get_logger().info("Узел overflow_l запущен и слушает топик!")
+        self.get_logger().info(f'Узел {self.node_2} запущен и слушает топик!')
 
    
     def callback(self, msg):
         self.get_logger().info(f"[WARN] [overflow_l]:!!! OVERFLOW !!! Resulting number is {msg.data}")
 
 def main():
-    rclpy.init()                    
-    node = overflow_l()            
+    rclpy.init()                   
+    node = overflow_l()              
     try:
-        rclpy.spin(node)           
+        rclpy.spin(node)            
     except KeyboardInterrupt:
         pass                        
     finally:
-        node.destroy_node()         
+        node.destroy_node()        
         rclpy.shutdown()            
 
 if __name__ == '__main__':
